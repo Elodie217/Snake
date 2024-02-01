c6074bbbcfa2f066c715d6665e659358d2dee288
Elodie <elodie.grienay.simplon@gmail.com>
2024-02-01 22:15:13 +0100

    Commit01/02

--- script.js
+++ script.js
@@ -1,34 +1,90 @@
 let body = document.body;
 let serpent = document.querySelector(".serpent");
-let x = 1;
-let y = 1;
-// serpent.style.gridArea = `1 / 1 / 2 / 2`;
+let x = 3;
+let y = 3;
+let interval = "";
+
 serpent.style.gridColumn = x;
 serpent.style.gridRow = y;
+
+function myStop() {
+  clearInterval(interval);
+}
 
 document.addEventListener("keydown", (event) => {
   let nomTouche = event.key;
 
   switch (nomTouche) {
     case "ArrowUp":
-      //   serpent.style.gridArea = `1 / 2 / 2 / 3`;
-      serpent.style.gridColumn = x;
-      serpent.style.gridRow = y - 1;
+      myStop();
+
+      interval = setInterval(() => {
+        y = y - 1;
+        mouvementserpent();
+      }, 100);
+
       break;
     case "ArrowDown":
-      //   serpent.style.gridArea = `2 / 1 / 3 / 2`;
-      serpent.style.gridColumn = x;
-      serpent.style.gridRow = y + 1;
+      myStop();
+
+      interval = setInterval(() => {
+        y = y + 1;
+        mouvementserpent();
+      }, 100);
+
       break;
     case "ArrowRight":
-      //   serpent.style.gridArea = `1 / 2 / 2 / 3`;
-      serpent.style.gridRow = x + 1;
-      serpent.style.gridColumn = y;
+      myStop();
+
+      interval = setInterval(() => {
+        x = x + 1;
+        mouvementserpent();
+      }, 100);
+
       break;
     case "ArrowLeft":
-      //   serpent.style.gridArea = `1 / 2 / 2 / 3`;
-      serpent.style.gridRow = x - 1;
-      serpent.style.gridColumn = y;
+      myStop();
+
+      interval = setInterval(() => {
+        x = x - 1;
+        mouvementserpent();
+      }, 100);
+
       break;
   }
 });
+
+function mouvementserpent() {
+  if (x <= 48 && x > 0 && y <= 25 && y > 0) {
+    serpent.style.gridColumn = x;
+    serpent.style.gridRow = y;
+  } else {
+    gameover();
+  }
+}
+
+////Fruits////
+function creerfruit() {
+  let fruit = document.querySelector(".fruit");
+
+  let fruitX = Math.floor(Math.random() * (48 - 1) + 1);
+  let fruitY = Math.floor(Math.random() * (25 - 1) + 1);
+
+  fruit.style.backgroundColor = "red";
+  fruit.style.gridColumn = fruitX;
+  fruit.style.gridRow = fruitY;
+}
+creerfruit();
+
+////Game Over////
+function gameover() {
+  myStop();
+  alert("T'es mort !");
+  reset();
+}
+
+function reset() {
+  x = 3;
+  y = 3;
+  mouvementserpent();
+}
